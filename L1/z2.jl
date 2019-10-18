6f3419fcf387af2a8d010707a5b6a48e52e5df66
Tomasz Benes <cansisti@outlook.com>
2019-10-18 12:51:37 +0200

    add comments l1/z2

--- L1/z2.jl
+++ L1/z2.jl
@@ -1,10 +1,12 @@
 # Tomasz Beneś
 # list 1, task 2
 
+# Oblicza macheps według wyrażenia Kahana dla danego typu t
 function calc_macheps_Kahan(t)
 	return t(3)*(t(4)/t(3)-t(1))-t(1)
 end
 
+# Macheps dla kolejnych typów
 ep16 = calc_macheps_Kahan(Float16)
 ep32 = calc_macheps_Kahan(Float32)
 ep64 = calc_macheps_Kahan(Float64)
